012440f94ea9d01717959e04d43f4d6b95c28c5a
brightfutureacad <brightfutureacad2025season01@gmail.com>
2025-09-03 08:28:12 +0530

    file path

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/Order");
+const Order = require("../../models/Order");
 
 // POST /api/orders
 router.post("/", async (req, res) => {
