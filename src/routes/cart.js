a2e9613f51a836419b05394eccbeb2836a24b5a3
brightfutureacad <brightfutureacad2025season01@gmail.com>
2025-09-03 08:28:12 +0530

    file path

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Cart = require("../models/Cart");
-const Product = require("../models/Product");
+const Cart = require("../../models/Cart");
+const Product = require("../../models/Product");
 
 // Add to cart
 router.post("/", async (req, res) => {
